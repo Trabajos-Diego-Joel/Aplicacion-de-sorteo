c9b44d953d6cc26a2e2d4b3b8db9575d05834b04
JOEL18VEGAS <joel18gonzalez@outlook.com>
2023-08-02 13:56:31 -0500

    Bugs testing.

--- src/pages/Rank.jsx
+++ src/pages/Rank.jsx
@@ -4,11 +4,11 @@ import { useEffect } from "react";
 
 import { createClient } from "@supabase/supabase-js";
 
-// SUpabase Connection.
 const supabase = createClient(
-  "https://rnakbdlosebdbhlnqdfz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuYWtiZGxvc2ViZGJobG5xZGZ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MDk5Njg0OCwiZXhwIjoyMDA2NTcyODQ4fQ.NNo5G0oRWxJCTUswBFkWYrGmB1Qtg_6tiqi_JX0_PsA"
+  "https://oyltvjfmloodupovoqoe.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95bHR2amZtbG9vZHVwb3ZvcW9lIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MDY2MjgyMSwiZXhwIjoyMDA2MjM4ODIxfQ.SwX21h80-C0yk9K19vk_UerdvH8CwVVSJRrHX9Q8MJA"
 );
+
 
 
 export default function Rank() {
